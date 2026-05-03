0d5e01a6bd16e1aff3378a506354c6376fdc9c49
猹 <183882190@qq.com>
2026-05-03 10:11:08 +0800

    Add files via upload

---
+++ js/forge加载.js
@@ -0,0 +1,95 @@
+(function ()
+{
+    if (location.host != "iirose.com")
+        return;
+    let doc = null;
+    let win = null;
+    if (location.pathname == "/")
+    {
+        doc = document;
+        win = window;
+    }
+    else if (location.pathname == "/messages.html")
+    {
+        doc = parent.document;
+        win = parent.window;
+    }
+    else
+        return;
+
+    if (win["iiroseForgeInjected"])
+        return;
+
+    let loaded = false;
+    let updatedCache = false;
+    let candidateUrl = [
+        "https://cdn.jsdelivr.net/gh/chara2580/WebView-iirose@main/js/forge.js"
+    ];
+
+    /**
+     * @param {string} url
+     */
+    async function tryUrl(url)
+    {
+        let response = await fetch(url, { cache: "no-cache" });
+        if (response.ok)
+        {
+            let codeStr = await response.text();
+            if (codeStr)
+            {
+                if (!loaded)
+                {
+                    loaded = true;
+                    console.log(`[iiroseForgeInjector] load from ${url}`);
+                    (new win.Function(codeStr))();
+                }
+                if (!updatedCache)
+                {
+                    updatedCache = true;
+                    let cache = await window?.["caches"]?.open?.("v");
+                    if (cache)
+                    {
+                        let cacheResponse = new Response(new Blob([codeStr], { type: "text/javascript" }), { status: 200, statusText: "OK" });
+                        cache.put(candidateUrl[0], cacheResponse);
+                        console.log(`[iiroseForgeInjector] cache updated`);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param {number} index
+     */
+    function tryCandidate(index)
+    {
+        tryUrl(candidateUrl[index]);
+
+        if (index < candidateUrl.length - 1)
+            setTimeout(() =>
+            {
+                if (!loaded)
+                    tryCandidate(index + 1);
+            }, 2 * 1000);
+    }
+
+    tryCandidate(0);
+
+    (async () =>
+    {
+        if (loaded)
+            return;
+
+        let cacheResponse = await (await window?.["caches"]?.open?.("v"))?.match(candidateUrl[0]);
+        if (cacheResponse && cacheResponse.ok)
+        {
+            let codeStr = await cacheResponse.text();
+            if (codeStr && !loaded)
+            {
+                loaded = true;
+                console.log(`[iiroseForgeInjector] load from cache`);
+                (new win.Function(codeStr))();
+            }
+        }
+    })();
+})();
